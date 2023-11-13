9ddbe075a9bfcd8e60ee064457023d349ecb4cc8
noe-lauras <lauras.noe@gmail.com>
2023-11-13 12:11:59 +0100

    ajout d'un clientWriter sur Server

--- src/Server.java
+++ src/Server.java
@@ -9,6 +9,8 @@ public class Server {
     private String ip;
     private int port;
     private boolean isRunning;
+    private PrintWriter clientWriter;
+
 
     public Server(String ip, int port) {
         this.ip = ip;
@@ -35,7 +37,7 @@ public class Server {
     private void handleClientConnection(Socket clientSocket) {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
 
             // Example: Read a message from the client
             String receivedMessage = reader.readLine();
@@ -44,8 +46,8 @@ public class Server {
             // si le client envoie "bye", on ferme la connexion
             if (receivedMessage.equals("bye")) {
                 this.isRunning = false;
-            
             }
+
             // Close the connection
             clientSocket.close();
 
@@ -53,39 +55,15 @@ public class Server {
             System.out.println("Error handling client connection: " + e.getMessage());
         }
     }
-
-
     public boolean isRunning() {
         return this.isRunning;
     }
 
     public void sendMessage(String message) {
-
-        try {
-            // on crée un socket pour se connecter au serveur
-            Socket clientSocket = new Socket(this.ip, this.port);
-
-            // on crée un writer pour envoyer un message au serveur
-            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            // on envoie le message au serveur
-            writer.println(message);
-
-            // on crée un reader pour lire la réponse du serveur
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            // on lit la réponse du serveur
-            String receivedMessage = reader.readLine();
-
-            // on affiche la réponse du serveur
-            System.out.println("Received from server: " + receivedMessage);
-
-            // on ferme la connexion
-            clientSocket.close();
-
-        } catch (Exception e) {
-            System.out.println("Error connecting to server: " + e.getMessage());
+        if (clientWriter != null) {
+            clientWriter.println(message);
+        }else{
+            System.out.println("No client connected");
         }
-
-    }
+    }   
 }
